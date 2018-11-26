0fcbdeeb64daa399e36ee1ef48b9138cd4fc1763
Salvador P <nyakebei.ipn@gmail.com>
2018-11-26 13:22:57 -0600

    Clarified Help

--- dataset_divide.m
+++ dataset_divide.m
@@ -4,7 +4,7 @@ function [dataset] = dataset_divide ( train_percent, valid_percent, test_percent
 %estructura resultante. Ejemplo:
 %   dataset.train.p -> input del conjunto de entrenamiento
 %   dataset.valid.t -> output del conjunto de validacion
-%   dataset.test    -> conjunto de prueba
+%   dataset.test    -> conjunto de prueba, contiene p y t
 
 p = load(dataset_input);
 t = load(dataset_output);
