298389f8342251f42c27a5b006320b86a403e56e
Salvador P <nyakebei.ipn@gmail.com>
2018-11-26 00:15:13 -0600

    Epoch validation function done

--- epoch_validation.m
+++ epoch_validation.m
@@ -1,15 +1,30 @@
-function [validation_error] = epoch_validation(P, T, W1, b1, W2, b2)
+function [validation_error] = epoch_validation(arch2, dataset, parameters)
 %Esta funcion ejecuta la validacion propagando los valores hacia adelante y
 %regresando el error de validación.
 
-a0 = P';
+%Pre-alocando a
+a = cell(1,length(parameters));
 
-a1 = logsig (W1 * a0 + b1);
+%Procesando cada capa
+for layer = 1:length(parameters)
+    if layer == 1
+        n = parameters(layer).w * dataset.p';
+    else
+        n = parameters(layer).w * a{layer-1};
+    end
+    switch arch2(layer)
+        case 1
+            a(layer) = {purelin(n)};
+        case 2
+            a(layer) = {logsig(n)};
+        case 3
+            a(layer) = {tansig(n)};
+    end
+end
 
-a2 = purelin(W2 * a1 + b2);
+final_dimension = [length(dataset.t) 1];
 
-a = a2;
+e = reshape(dataset.t, final_dimension) - reshape ( a{length(parameters)}, final_dimension);
 
-e = T - a;
+validation_error = sum(e)/length(e);
 
-validation_error = sum(e, 'all') / length(T);
