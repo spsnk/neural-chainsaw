3c5ff8ab5abb62f16f80cab6b5eca6a71e8f19c6
Salvador P <nyakebei.ipn@gmail.com>
2018-11-25 18:02:24 -0600

    Fixed correct size calculation for test set

--- dataset_divide.m
+++ dataset_divide.m
@@ -1,4 +1,10 @@
 function [dataset] = dataset_divide ( train_percent, valid_percent, test_percent, dataset_input, dataset_output )
+%Divide un dataset (en texto plano) dado en subconjuntos de entrenamiento 
+%validacion y prueba. Regresa y guarda en archivo 'dataset.mat' la
+%estructura resultante. Ejemplo:
+%   dataset.train.p -> input del conjunto de entrenamiento
+%   dataset.valid.t -> output del conjunto de validacion
+%   dataset.test    -> conjunto de prueba
 
 p = load(dataset_input);
 t = load(dataset_output);
@@ -15,8 +21,8 @@ t(1:ceil(dataset_size/valid_size):dataset_size) = [];
 
 dataset.test.p = p(1:ceil(length(p)/test_size):length(p));
 p(1:ceil(length(p)/test_size):length(p)) = [];
-dataset.test.t = t(1:ceil(length(p)/test_size):length(p));
-t(1:ceil(length(p)/test_size):length(p)) = [];
+dataset.test.t = t(1:ceil(length(t)/test_size):length(t));
+t(1:ceil(length(t)/test_size):length(t)) = [];
 
 dataset.train.p = p;
 dataset.train.t = t;
