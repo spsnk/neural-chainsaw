d9c9b0d6e080f12dda8f008219c561b0c1312a4a
Salvador P <nyakebei.ipn@gmail.com>
2018-11-25 18:03:04 -0600

    Added function description

--- dataset_generate.m
+++ dataset_generate.m
@@ -1,4 +1,6 @@
 function [input,output] = dataset_generate(i,samples)
+%Genera un dataset simple de una funcion senoidal y guarda archivos
+%'dataset_input.txt' y 'dataset_output.txt'.
 
 input = (-2:1/(samples/4):2)';
 output = 1 + sin((pi*i*input)/4);
