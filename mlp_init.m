360897412ccf309bcb153ffcf632bae76320fc3c
Salvador P <nyakebei.ipn@gmail.com>
2018-11-25 19:58:21 -0600

    Corrected size generation for correct matrix operations

--- mlp_init.m
+++ mlp_init.m
@@ -6,8 +6,15 @@ function [parameters] = mlp_init (arquitecture)
 parameters = struct ( 'w', cell([1 2]), 'b', cell([1 2]));
 
 for i=2:length(arquitecture)
-   parameters(i-1).w = -1 + 2.*rand([1 (arquitecture(i) * arquitecture(i-1))]);
-   parameters(i-1).b = -1 + 2.*rand([1 arquitecture(i)]);
+   if mod(i,2)
+       dimsw = [1 (arquitecture(i) * arquitecture(i-1))];
+       dimsb = [1 arquitecture(i)];
+   else
+       dimsw = [(arquitecture(i) * arquitecture(i-1)) 1];
+       dimsb = [arquitecture(i) 1];
+   end
+   parameters(i-1).w = -1 + 2.*rand(dimsw);
+   parameters(i-1).b = -1 + 2.*rand(dimsb);
 end
 
 save('parameters.mat','parameters');
