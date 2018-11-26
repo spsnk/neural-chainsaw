2ce18789de38681e583643510a0c030354a1d65d
Salvador P <nyakebei.ipn@gmail.com>
2018-11-26 13:30:58 -0600

    Changed 'parameters' to 'parameter' for consistency

--- mlp_init.m
+++ mlp_init.m
@@ -1,9 +1,9 @@
-function [parameters] = mlp_init (arquitecture)
+function [parameter] = mlp_init (arquitecture)
 %Inicializa los pesos y bias basado en la arquitectura dada como argumento
 % por ejemplo: v1 = [1 2 1];
 %Regresa y guarda los valores en el archivo 'parameters.mat'
 
-parameters = struct ( 'w', cell([1 2]), 'b', cell([1 2]));
+parameter = struct ( 'w', cell([1 2]), 'b', cell([1 2]));
 
 for i=2:length(arquitecture)
    if mod(i,2)
@@ -13,8 +13,8 @@ for i=2:length(arquitecture)
        dimsw = [(arquitecture(i) * arquitecture(i-1)) 1];
        dimsb = [arquitecture(i) 1];
    end
-   parameters(i-1).w = -1 + 2.*rand(dimsw);
-   parameters(i-1).b = -1 + 2.*rand(dimsb);
+   parameter(i-1).w = -1 + 2.*rand(dimsw);
+   parameter(i-1).b = -1 + 2.*rand(dimsb);
 end
 
-save('parameters.mat','parameters');
+save('parameters.mat','parameter');
