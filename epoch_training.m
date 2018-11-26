bb5e2ec1c51e9218867a119ac07c90ed566b9ae0
Salvador P <nyakebei.ipn@gmail.com>
2018-11-25 19:47:15 -0600

    Writing vectorized training epoch function

--- epoch_training.m
+++ epoch_training.m
@@ -1,15 +1,28 @@
-function [valerror] = epoch_training(v1, v2)
+function [epoch_error] = epoch_training(arch2, dataset, parameters)
 %Esta Funcion ejecuta una época propagando los valores hacia adelante y
 %realizando backpropagation, regresando el error de epoca.
 
-a0 = P';
+%Pre-alocando a
+a = zeros(1,length(parameters));
 
-a1 = logsig (W1 * a0 + b1);
+%Procesando cada capa
+for layer = 1:length(parameters)
+    if mod(layer,2)
+        w = parameters(layer).w;
+        p = dataset.p;
+    else
+        w = parameters(layer).w';
+        p = dataset.p';
+    end
+    n = w * p;
+    switch arch2(layer)
+        case 1
+            a(layer) = purelin(n);
+        case 2
+            a(layer) = logsig(n);
+        case 3
+            a(layer) = tansig(n);
+    end
+end
 
-a2 = purelin(W2 * a1 + b2);
-
-a = a2;
-
-e = T - a;
-
-valerror = sum(e, 'all') / length(T);
+epoch_error = sum(e, 'all') / length(T);
