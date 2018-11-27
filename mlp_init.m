de5cbc089281c87d652bd98fec7fcc917fe7a021
Salvador P <nyakebei.ipn@gmail.com>
2018-11-26 18:05:12 -0600

    Cleanup

--- mlp_init.m
+++ mlp_init.m
@@ -10,13 +10,6 @@ for i=2:length(arquitecture)
        dimsw = [arquitecture(i) 1];
    else
        dimsw = [arquitecture(i)  arquitecture(i-1)];
-%        if mod(i,2)
-%            dimsw = [1 (arquitecture(i) * arquitecture(i-1))];
-%            dimsb = [1 arquitecture(i)];
-%        else
-%            dimsw = [(arquitecture(i) * arquitecture(i-1)) 1];
-%            dimsb = [arquitecture(i) 1];
-%        end
    end
    dimsb = [arquitecture(i) 1];
    parameter(i-1).w = -1 + 2.*rand(dimsw);
